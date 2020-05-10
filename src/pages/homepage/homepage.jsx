a063433ff9b52e7430904631881618002e568c23
aishmn <manish.sinuwari@gmail.com>
2020-05-10 22:19:10 +0545

    Code Destruction

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.scss";
+import Directory from "../../components/directory/directory";
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
